6ca0222c1c70d9f0032f72ebc54a4326ca6433d6
Donald149 <fongkapak@gmail.com>
2023-09-19 14:56:08 -0700

    Added notes to NLP and added Python file

---
+++ input_and_output.py
@@ -0,0 +1,8 @@
+# Input and Output
+# Author: Donald
+# Date: 19 September 2023
+
+# Output a quote from someone
+print("Git is Google Drive on caffeine. --Mr.Ubial, probably")
+print("I refuse your refusal. --Luffy")
+print("The greater the mass, the greater the force of attraction. --Some science guy")
