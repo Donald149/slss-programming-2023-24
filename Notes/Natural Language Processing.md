73a01b32c07c65be6b279c36473c7e5bd7d8526e
Donald149 <fongkapak@gmail.com>
2023-09-25 09:47:30 -0700

    edited chatbot and added notes

--- Notes/Natural Language Processing.md
+++ Notes/Natural Language Processing.md
@@ -77,3 +77,6 @@ aaaa
 
 # [[Design]]
 
+# [[Lists]]
+
+# [[Modules]]
--- chatbot.py
+++ chatbot.py
@@ -2,18 +2,27 @@
 # Author: Donald
 # Date: 20 September 2023
 
+import random
+import time
+
 # Greet the user
 print("Greetings.")
+time.sleep(1.5)
 print("I am a chatbot, hoping to learn more about humans.")
+time.sleep(2)
 
 # Get the user's name and store it in a variable
 user_name = input("To begin with, what's your name? ")
 
 # Respond with the user's name in a friendly way
+time.sleep(1.5)
 print(f"Great. I look forward to having a great conversation with you, {user_name}.")
+time.sleep(2)
 
 # Ask the user what their favourite food is
-fav_food = input("Let's see... what food do you like the most? ")
+print("I want to know more about you...")
+time.sleep(1)
+fav_food = input("What's your favorite food? ")
 
 # Make a comment about their food but have multiple dialogue options
 # Create a list of possible responsees
@@ -26,8 +35,8 @@ list_food_dialogue = [
 ]
 
 # Choose one of those responses randomly
-import random
 random_food_dialogue = random.choice(list_food_dialogue)
 
 # Print out that chosen response
+time.sleep(2)
 print(random_food_dialogue)
