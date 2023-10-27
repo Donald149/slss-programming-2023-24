9428f69ba1049825d56a2a2d35ef1de171748f9d
Donald149 <fongkapak@gmail.com>
2023-10-27 09:09:54 -0700

    added more to number_practice

--- number_practice.py
+++ number_practice.py
@@ -1,3 +1,16 @@
 # Number Practice
 # Author: Donald
 # Date: 26 October 2023
+
+# Operators that work on Numbers
+print(1 + 1)
+print(3 - 2)
+print(10 * 10)    # Multiply
+print(9 / 3)      # Division
+
+print(1 + 2 / 4 + 5 - 4)    # BEDMAS
+
+print(9 // 2)    # Floor division (Rounds down)
+print(9  % 2)    # Modulo gives remainder
+
+print(2 ** 4)    # Power function
