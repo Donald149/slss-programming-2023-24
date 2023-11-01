955c4dc08edb61c0f4e697907baf2e05e662a385
Donald149 <fongkapak@gmail.com>
2023-11-01 15:01:05 -0700

    added practices

---
+++ age_in_2049.py
@@ -0,0 +1,12 @@
+# Age in 2049
+# Author: Donald
+# 1 November 2023
+
+# Ask for current user age
+current_user_age = int(input("How old are you? "))
+
+# math
+current_year = 2023
+age_in_2049 = 2049 - current_year + current_user_age
+
+print(f"In 2049 you will be {age_in_2049} years old.")
---
+++ olympic_judging.py
@@ -0,0 +1,19 @@
+# Olympic Jumping
+# Author: Donald
+# 1 November 2023
+
+judges = 5
+
+# Ask for judge scores
+print("Judges please give a score.")
+
+total_score = 0
+
+for _ in range(judges):
+    added_score = int(input(f"Judge {_ + 1}: ").strip(",!?"))
+    total_score += added_score
+
+# calc average score
+average_score = total_score / judges
+
+print(f"Your Olympic score is {average_score}.")
