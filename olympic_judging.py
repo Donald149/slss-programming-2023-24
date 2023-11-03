0cad827c5a576013b90d1d63dd5698ceb19e9644
Donald149 <fongkapak@gmail.com>
2023-11-03 08:44:27 -0700

    made changes to olympic_judging.py

--- olympic_judging.py
+++ olympic_judging.py
@@ -1,4 +1,4 @@
-# Olympic Jumping
+# Olympic Judging
 # Author: Donald
 # 1 November 2023
 
@@ -10,10 +10,10 @@ print("Judges please give a score.")
 total_score = 0
 
 for _ in range(judges):
-    added_score = int(input(f"Judge {_ + 1}: ").strip(",!?"))
+    added_score = float(input(f"Judge {_ + 1}: ").strip(",!?"))
     total_score += added_score
 
 # calc average score
 average_score = total_score / judges
 
-print(f"Your Olympic score is {average_score}.")
+print(f"Your Olympic score is {average_score:.1f}")
