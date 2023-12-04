0f460a7897e8346561e43f668947a73c78ef7f7f
Donald149 <fongkapak@gmail.com>
2023-12-04 14:29:36 -0800

    tree

--- functions_turtle.py
+++ functions_turtle.py
@@ -13,6 +13,7 @@ def squared(num: float) -> float:
 
     return num ** 2
 
+
 def draw_square(side_length: int) -> None:
     """Draws a square of a given length."""
     burt.forward(side_length)
@@ -24,12 +25,46 @@ def draw_square(side_length: int) -> None:
     burt.forward(side_length)
     burt.left(90)
 
+
+def draw_tree(level: int, height: int) -> None:
+    """A recursive function that draws a tree 
+    with initial given height in pixels
+    
+    Params:
+
+    level - number representing the levels of branches
+    height - height of the main tree in pixels
+    """
+
+    if level > 0:
+        # 1. Move turtle forward height pixels
+        burt.forward(height)
+
+        # 2. Turn turtle left
+        burt.left(36)
+        #   a. Draw a smaller tree (current level - 1)
+        draw_tree(level - 1, height * 0.75)
+        
+        # 3. Turn turtle right
+        burt.right(36 * 2)
+        #   a. Draw a smaller tree (current level - 1)
+        draw_tree(level - 1, height * 0.75)
+
+        # 4. Return to beginning
+        burt.left(36)
+        burt.back(height)
+    else:
+        original_colour = burt.color()
+        burt.color("green")
+        burt.stamp()
+        burt.color(original_colour[0]) # revert colour
+
+# Set-up Burt to draw trees
+burt.color("brown")
+burt.setheading(90)
+burt.width(4)
 burt.speed(0)
 
-# Draw squares that grow exponentially
-for i in range (40):
-    draw_square(squared(i))
-
-
+draw_tree(10, 175)
 
 turtle.done()
