5a4c887e700cffdc644bbb5c05874948f1e2592e
Donald149 <fongkapak@gmail.com>
2023-12-13 11:52:51 -0800

    made colour_helper.py

---
+++ colour_helper.py
@@ -0,0 +1,22 @@
+# Colour helper
+# Author: Donald
+# 13 December 2023
+
+# Need help with colour?
+# This shall help you!
+
+
+def pixel_to_string(pixel:tuple) -> str:
+    """Take a rgb 3-tuple and "interpret it"
+    as a colour and return that colour's name
+    
+    Params:
+        pixel - 3-tuple of (red, green, blue)
+
+    Return:
+        String representing the colour
+    """
+    r, g, b = pixel
+
+    if g > 250 and r < 32 and b < 32:
+        return "green"
