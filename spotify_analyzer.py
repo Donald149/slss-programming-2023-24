a2adf077a94bae82c1de49eeee6c2ae601dbb2a2
Donald149 <fongkapak@gmail.com>
2024-01-16 09:49:53 -0800

    made changes to music finder

--- spotify_analyzer.py
+++ spotify_analyzer.py
@@ -4,45 +4,65 @@
 
 import csv
 
-# Open
-with open("./spotify.csv") as f:
-    # ----- Search  for all Drake Songs
-    # ----- Use linear search
-    # Create a csv reader object
-    csv_reader = csv.DictReader(f)
+# Create a function that searches through data
+# Finds all songs from a given artist
 
-    # Make a list to store all of Drake's songs
-    drake_songs = []
 
-    # Create a counter to store thee current line number
-    line_num = 0
+def find_all_songs(artist: str) -> list:
+    """Searches through a set of data and 
+    returns all songs found from a given artist
 
-    # For every line in file
-    for line in csv_reader:
-        # First line -> print header
-        if line_num == 0:
-            print("The columns are: ")
+    Returns:
+        A list found songs by given artist in tuples
+        (artist, song name, danceability)
+    """
 
-            # Print on one line:
-            print(", ".join(line))
-            
-            # Print one on every line
-            # for item in line:
-                # print(item)
-            
-            line_num += 1
-        else:
-            # If the artist is who we looking for
-            # Store the song info in the list
-            # ----- artist, song title, dancability
-            if "drake" in line['artist'].lower():
-                drake_songs.append((
-                    line['artist'],
-                    line['song_title'],
-                    line['danceability']
-                ))
-            line_num += 1
+    # Open
+    with open("./spotify.csv") as f:
+        # ----- Search for all artist Songs
+        # ----- Use linear search
+        # Create a csv reader object
+        csv_reader = csv.DictReader(f)
 
-    for song in drake_songs:
-        if float(song[-1]) > 0.5:
-            print(song)
+        # Make a list to store all of artist's songs
+        songs = []
+
+        # Create a counter to store thee current line number
+        line_num = 0
+
+        # For every line in file
+        for line in csv_reader:
+            # First line -> print header
+            if line_num == 0:
+                # print("The columns are: ")
+
+                # Print on one line:
+                # print(", ".join(line))
+                
+                # Print one on every line
+                # for item in line:
+                    # print(item)
+                
+                line_num += 1
+            else:
+                # If the artist is who we looking for
+                # Store the song info in the list
+                # ----- artist, song title, dancability
+                if artist.lower() in line["artist"].lower():
+                    songs.append(
+                        (line["artist"], line["song_title"], line["danceability"])
+                    )
+                line_num += 1
+
+    return songs
+
+
+drake_songs = find_all_songs("drake")
+ed_sheeran_songs = find_all_songs("ed sheeran")
+
+# Print the songs that have 
+# a certain dancibility or higher
+
+for song in ed_sheeran_songs:
+    if float(song[-1]) > 0.5:
+        print(song)
