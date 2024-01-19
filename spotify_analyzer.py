6469929d59484bae4cd62dc216775b61cf30490f
Donald149 <fongkapak@gmail.com>
2024-01-19 14:36:53 -0800

    sorter

--- spotify_analyzer.py
+++ spotify_analyzer.py
@@ -47,10 +47,10 @@ def find_all_songs(artist: str) -> list:
             else:
                 # If the artist is who we looking for
                 # Store the song info in the list
-                # ----- artist, song title, dancability
+                # ----- artist, song title, danceability
                 if artist.lower() in line["artist"].lower():
                     songs.append(
-                        (line["artist"], line["song_title"], line["danceability"])
+                        (line["artist"], line["song_title"], line["valence"], line["danceability"])
                     )
                 line_num += 1
 
@@ -61,8 +61,47 @@ drake_songs = find_all_songs("drake")
 ed_sheeran_songs = find_all_songs("ed sheeran")
 
 # Print the songs that have 
-# a certain dancibility or higher
+# a certain danceability or higher
 
-for song in ed_sheeran_songs:
-    if float(song[-1]) > 0.5:
-        print(song)
+# Starting at the beginning of the list until the end
+for i, song in enumerate(drake_songs):
+    # set the current item to the smallest danceability
+    smallest_danceability = song[-1]
+    smallest_idx = i
+    
+    # For the rest of the list
+    for j in range(i +1, len(drake_songs)):
+        # If the current item is smaller than the smallest
+        if drake_songs[j][-1] < smallest_danceability:
+            # set the smallest with the current
+            smallest_danceability = drake_songs[j][-1]
+            smallest_idx = j
+
+    # swap the smallest with the current
+    drake_songs[i], drake_songs[smallest_idx] = (
+        drake_songs[smallest_idx], drake_songs[i]
+    )
+
+for songs in drake_songs:
+    print(songs)
+
+for i, song in enumerate(ed_sheeran_songs):
+    # set the current item to the smallest danceability
+    greatest_danceability = song[-1]
+    greatest_idx = i
+    
+    # For the rest of the list
+    for j in range(i +1, len(ed_sheeran_songs)):
+        # If the current item is smaller than the smallest
+        if ed_sheeran_songs[j][-1] < greatest_danceability:
+            # set the smallest with the current
+            greatest_danceability = ed_sheeran_songs[j][-1]
+            greatest_idx = j
+
+    # swap the smallest with the current
+    ed_sheeran_songs[i], ed_sheeran_songs[greatest_idx] = (
+        ed_sheeran_songs[greatest_idx],ed_sheeran_songs[i]
+    )
+
+for songs in ed_sheeran_songs:
+    print(songs)
